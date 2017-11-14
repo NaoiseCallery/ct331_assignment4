512170c2e3dcfb3dbe848beae11d360e446d264d
Naoise Callery <naoisecallery@gmail.com>
2017-11-14 22:20:42 +0000

    Done
    Finished all questions.

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,25 @@
 %Definitions for isElementInList(El, List)
 
+isElementInList(X, [X|T]).
+isElementInList(X, [H|T]):-
+isElementInList(X, T).
+		  
+%Definitions for mergeLists(List1, List2, Merged)
+
+mergeLists([], Y, Y).
+mergeLists([H|[]], Y, [H|Y]).
+mergeLists([H|T], Y, Conc) :- 
+mergeLists(T, Y, C), 
+mergeLists([H], C, Conc).
+	
 %Definitions for reverseList(List, ReversedList)
+
+reverseList([],[]).
+reverseList([H|T], Y):-
+reverseList(T, Z), 
+mergeLists(Z, [H], Y).
 
 %insertElementIntoListEnd(El, List, NewList)
 
-%Definitions for mergeLists(List1, List2, Merged)
+insertElementIntoListEnd(X, Y, Z) :- 
+mergeLists(Y, [X], Z).
